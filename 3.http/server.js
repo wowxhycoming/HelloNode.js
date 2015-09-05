5edb6291f50bb95c739eee771d3611c51cd9cda3
wowxhycoming <wowxhycoming@sina.com>
2015-09-06 00:05:47 +0800

    a simple commit

--- 3.http/server.js
+++ 3.http/server.js
@@ -2,3 +2,17 @@
  * Created by xuhuaiyu on 2015/9/5.
  */
 
+var http = require('http');
+var server = http.createServer(serverProcess);
+
+function serverProcess(req,res) {
+
+    var url = req.url;
+    console.log(url);
+    // 如果访问为  http://127.0.0.1:8080/a/b/c/s/d/d/g/a/s  ， 输出  /a/b/c/s/d/d/g/a/s
+
+
+    res.end("nice");
+}
+
+server.listen(8080,'127.0.0.1');
