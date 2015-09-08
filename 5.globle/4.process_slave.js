144083c42d1cd483d12729c10b351d88e060bdef
wowxhycoming <wowxhycoming@sina.com>
2015-09-09 00:34:34 +0800

    daily push

--- 5.globle/4.process_slave.js
+++ 5.globle/4.process_slave.js
@@ -13,3 +13,13 @@ process.stdin.on('data', function(chunk) {
 });
 
 
+// 不想被随意kill的slave，处理不同的kill类型的信号量
+// 来自键盘的信号量
+process.on('SIGINT',function() {
+    console.log("接收到来自键盘kill进程的信号量，但是并没有什么卵用");
+});
+
+//
+process.on('SIGTERM',function() {
+    console.log("接收到来自键盘kill进程的信号量，但是并没有什么卵用");
+});
