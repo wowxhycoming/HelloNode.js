dc29834f2910e643b9ecb7a309ced4c90d98fc0a
wowxhycoming <wowxhycoming@sina.com>
2015-09-25 17:33:42 +0800

    daily commit

--- 6.event/5.2.prototype.js
+++ 6.event/5.2.prototype.js
@@ -22,9 +22,58 @@ function Person() {
 Person.prototype.name = 'person1';
 Person.prototype.showName = function() {
     console.log(this.name);
-    console.log(this.name === this.__proto__.name); // true 只有构造函数才有prototype属性
+    console.log(this.name === this.__proto__.name); // true 只有构造函数才有prototype属性 ，说明原型对象的name是一样的
 };
 
 var person1 = new Person();
 person1.showName(); // this.name 中的 this指的是 person1
+
+
+function Animal(name , food) {
+    this.name = name;
+    this.food = food;
+    this.getName = function() {
+      return this.name;
+    };
+}
+
+Animal.prototype.food = 'meat';
+Animal.prototype.eat = function () {
+    console.log('I an eating ' + this.food);
+};
+
+var tiger = new Animal('tiger');
+var mouse = new Animal('mouse', 'rice');
+// false ： 在创建Animal的两个实例tiger和mouse的时候，getName是被重复创建的， 他们之间是不相等的
+console.log(tiger.getName ==  mouse.getName);
+
+// true ：原型上创建的属性是共用的，不会被重复创建
+console.log(tiger.eat == mouse.eat);
+
+console.log(tiger.getName()); // 返回函数执行结果
+console.log(tiger.getName); // 返回属性，是个函数对象[Function]
+
+// 原型上有个food、对象内部也有food。内部私有变量会覆盖原型上的变量，创建tiger的时候又没有传入food，就是undefined
+tiger.eat(); // I an eating undefined
+mouse.eat(); // I an eating rice
+
+function a() {
+    var a = 'a';
+    function b() {
+        console.log(a);
+        var a = undefined; // 在b()的作用域内，定义了变量a，并且会预解释，预解释的范围是整个函数内部。
+    }
+    b();
+}
+a();
+
 // ==== 解释3
+
+/**
+ * 原型链
+ * Function 、 Object 都是构造函数，用于生成对象
+ * 所有的构造函数，包括Object都是Function构造函数的实例
+ * Object.prototype 是所有对象的祖先
+ * Function.prototype 是所有函数的祖先
+ *
+ */
