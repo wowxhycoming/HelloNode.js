775d2240fd71278862459becce1b2ae6f84db0bd
wowxhycoming <wowxhycoming@sina.com>
2015-10-02 14:10:58 +0800

    daily push

---
+++ 8.buffer/2.transform.js
@@ -0,0 +1,14 @@
+/**
+ * Created by xuhuaiyu on 2015/10/1.
+ */
+
+console.log(parseInt("e7",16)); // 16进制转10进制
+console.log(parseInt("777",8)); // 8进制转10进制
+console.log(parseInt("00111001",2)); // 2进制转10进制
+
+console.log((100).toString(2)); //把100进制转成2进制
+console.log((100).toString(8)); //把100进制转成8进制
+console.log((100).toString(16)); //把100进制转成16进制
+
+
+
