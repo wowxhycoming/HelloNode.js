52b31db49e2b212e7d5d0ca1a096a97a8c076320
wowxhycoming <wowxhycoming@sina.com>
2015-10-24 15:05:11 +0800

    daily commit

--- 11.tcp_udp/8.chatServer.js
+++ 11.tcp_udp/8.chatServer.js
@@ -12,46 +12,55 @@
 
 var net = require('net');
 var util = require('util');
-
+// TODO 为啥 数组[元素] = obj 的时候会有空元素在数组里
 var clients = []; // 存放所有的用户
 // 也可以定义成 clients = {}  移除的时候用 delete clients[key]
 
 var server = net.createServer(function(socket) {
-    console.log(clients);
     var nickName;
-    socket.write('欢迎，请输入用户名\r\n');
+    socket.write('欢迎，请输入昵称\r\n');
     socket.setEncoding('utf8');
+    console.log(clients);
     socket.on('data', function(chunk){
         if(nickName){
             broadcast(null, nickName + " ： " + chunk);
         } else {
+            console.log(clients);
             if(clients[nickName]){
-                socket.write('用户名已被占用，请重新输入用户名\r\n');
+                socket.write('昵称已被占用，请重新输入昵称\r\n');
             } else {
                 chunk = chunk.replace('\r\n', '');
                 nickName = chunk;
                 clients[nickName] = socket; // 保存用户名和socket的关系
                 // 广播加入
-                broadcast(nickName,"系统： " + nickName + " 加入聊天室，目前有" + clients.length-1 + '人在聊天室');
+                console.log(clients);
+                broadcast(nickName, '系统： ' + nickName + ' 加入聊天室，目前有' + (clients.length - 1) + '人在聊天室');
             }
         }
     });
 
     socket.on('close', function() {
-        clients.remove(clients);
+        delete clients[nickName];
+        // 客户端发出close请求，socket已经关闭了，不能再对socket进行操作了。
         socket.destroy();
-        broadcast(nickName,"系统： " + nickName + " 离开聊天室，目前有" + clients.length-1 + '人在聊天室');
+        broadcast(nickName, '系统： ' + nickName + ' 离开聊天室，目前有' + (clients.length-1) + '人在聊天室');
     });
 
-    function broadcast(nickName, msg) {
-        msg += '\r\n';
-        for(var name in clients) {
-            if(name != nickName) {
-                clients[name].write(msg);
-            }
+    socket.on('error', function(error){
+        console.log(error);
+    });
+
+
+});
+
+function broadcast(nickName, msg) {
+    msg += '\r\n';
+    for(var name in clients) {
+        if(name != nickName) {
+            clients[name].write(msg);
         }
     }
-});
+}
 
 server.listen(9000);
 
