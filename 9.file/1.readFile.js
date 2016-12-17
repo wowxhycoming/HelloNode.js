2a1c5560ab21e100b60b36c97e83a1aa80a20d60
xuhuaiyu <wowxhycoming@sina.com>
2016-12-17 18:28:13 +0800

    daily push

--- 9.file/1.readFile.js
+++ 9.file/1.readFile.js
@@ -14,6 +14,13 @@ console.log(content);
  * 异步方式读取文件
  * 尽量使用异步的方式，只有当需要读取文件作为前置条件的时候才用同步读取
  */
+/**
+ * API
+ * fs.readFile(path,option,callback)
+ * path : 文件路径
+ * option : 如果是字符串，则被设置成字符集，读写模式默认为读；
+ * callback : 回调函数
+ */
 fs.readFile('msg.txt','utf8',function(err,data){
     console.log(err);
     console.log(data);
--- 9.file/4.open.js
+++ 9.file/4.open.js
@@ -2,35 +2,51 @@
  * Created by xuhuaiyu on 2015/10/2.
  */
 
-/**
- * 如何从指定的位置开始读取文件
- * 0 stdin 标准输入
- * 1 stdout 标准输出
- * 2 stderr 错误输出
- **/
 var fs = require('fs');
-//fd 是打开文件的索引
-fs.open('msg.txt','r',function(err,fd){
-    console.log(fd);
+/**
+ * fs.open(path, flags, mode, callback_)
+ * path : 文件路径
+ * flags : 模式，r读、w写、a追加
+ * mode : 权限
+ * callback_(err, fd) : fs会取参数列表中最后一个参数作为回调函数。
+ *  err : 错误信息
+ *  fd : 文件描述符，这是一个开发文件的索引，是累加的
+ *   0 stdin 标准输入
+ *   1 stdout 标准输出
+ *   2 stderr 错误输出
+ *   3 打开一个文件（开发文件 +1，关闭文件 -1）
+ *   4 又打开一个文件
+ */
+fs.open('msg.txt','r',function(err,fd) {
+    console.log("open 1:",err, fd);
+    fs.close(fd); // 关闭fd
+    fs.open('msg.txt','r',function(err,fd){
+        console.log("open 2:",err, fd);
+    });
 });
-/*
+
+
+/**
  * 读取文件 可以多次读取，每次读一小部分
+ * fs.read(fd, buffer, offset, length, position, callback)
  * fd 文件描述符
  * buffer 读到哪个buffer里
- * offset buffer偏移量
- * length 写多少个字节
+ * offset buffer偏移量，从buffer的哪里开始写
+ * length 读写多少个字节。可能发生读不满的情况
  * position 从文件的哪个位置开始读
- * callback
+ * callback(err,bytesRead,buf)
+ *  err : 错误信息
+ *  bytesRead : 实际读到的字节数
+ *  buf :
  */
-fs.open('msg.txt','r',function(err,fd){
+fs.open('msg.txt','r',function(err,fd) {
     var buffer = new Buffer(9);
     fs.read(fd,buffer,0,6,3,function(err,bytesRead,buf){
-        console.log(buffer == buf); // true
         console.log(buffer === buf); // true 就是同一个对象
-        console.log(bytesRead); // 于length相同
-        console.log(buffer.toString());
+        console.log(bytesRead); // 实际读取到的长度
+        console.log(buffer.slice(0,bytesRead).toString()); // 没有读满buffer
         fs.read(fd,buffer,6,3,9,function(err,bytesRead,buf){
-
+            console.log(buffer.toString());
         });
     });
 });
